08d3dee53030e4d342e7f72ce633b9af4fa9f59f
loidinhm31 <loidinh.git@gmail.com>
2024-05-02 01:49:38 +0700

    Fix to build in vite for mediapipe

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "node:path";
+import * as fs from "node:fs";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+function mediaPipeExportsWorkaround() {
+  return {
+    name: "mediapipe_workaround",
+    load(id) {
+      if (path.basename(id) === "face_mesh.js") {
+        console.log("mediapipe workaround");
+        let code = fs.readFileSync(id, "utf-8");
+        code += "exports.FaceMesh = FaceMesh;";
+        return { code };
+      } else {
+        return null;
+      }
+    }
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/vite-deploy/",
-  include: ['node_modules/@mediapipe/face_mesh/**/*'],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "node_modules/@mediapipe/face_mesh/**/*",
+          dest: "node_modules/@mediapipe/face_mesh"
+        }
+      ]
+    })
+  ],
+  base: "/",
+  build: {
+    rollupOptions: {
+      plugins: [mediaPipeExportsWorkaround()]
+    }
+  }
+});
