8d1f3bb8fa36a08b752bbeb95cb5c95f3f975166
loidinhm31 <loidinh.git@gmail.com>
2024-05-02 01:04:08 +0700

    Add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/vite-deploy/",
   include: ['node_modules/@mediapipe/face_mesh/**/*'],
 })
